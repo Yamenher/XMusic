fb4243f380c5fb7db3fceadeda4dc27e7d9beee4
syntaxspin <sketcherwaree@gmail.com>
2025-10-12 23:26:14 +0100

    refactor : settings.xml with Material 3 expressive list

--- app/src/main/java/com/xapps/media/xmusic/settingsFragment.java
+++ app/src/main/java/com/xapps/media/xmusic/settingsFragment.java
@@ -3,7 +3,10 @@ import android.os.*;
 import android.view.*;
 import androidx.annotation.*;
 import androidx.fragment.app.Fragment;
+import dev.trindadedev.fastui.preferences.withicon.Preference;
 import com.xapps.media.xmusic.databinding.SettingsBinding;
+
+import java.util.Objects;
 
 public class settingsFragment extends Fragment {
     private SettingsBinding binding; 
@@ -17,7 +20,61 @@ public class settingsFragment extends Fragment {
 	}
 
     private void initializeLogic() {
+
         setupListeners();
+
+        setupItems();
+    }
+
+    private void setupItems() {
+        binding.linear1.addView(appearanceSetting());
+        binding.linear1.addView(placeHolderSetting());
+        binding.linear1.addView(placeHolder2Setting());
+        binding.linear1.addView(placeHolder3Setting());
+    }
+
+    private Preference appearanceSetting() {
+        Preference pref = new Preference(requireContext());
+        pref.setIcon(R.drawable.ic_palette);
+        pref.setTitle("Appearance");
+        pref.setDescription("Customize XMusic UI");
+        pref.setBackgroundType(0);
+        return pref;
+
+
+    }
+
+    private Preference placeHolderSetting() {
+        Preference prefs = new Preference(requireContext());
+        prefs.setIcon(R.drawable.ic_android);
+        prefs.setTitle("PlaceHolder Title");
+        prefs.setDescription("PlaceHolder Description");
+        prefs.setBackgroundType(1);
+        return prefs;
+
+
+    }
+
+    private Preference placeHolder2Setting() {
+        Preference prefs = new Preference(requireContext());
+        prefs.setIcon(R.drawable.ic_android);
+        prefs.setTitle("PlaceHolder2 Title");
+        prefs.setDescription("PlaceHolder2 Description");
+        prefs.setBackgroundType(1);
+        return prefs;
+
+
+    }
+
+    private Preference placeHolder3Setting() {
+        Preference prefs = new Preference(requireContext());
+        prefs.setIcon(R.drawable.ic_android);
+        prefs.setTitle("PlaceHolder3 Title");
+        prefs.setDescription("PlaceHolder3 Description");
+        prefs.setBackgroundType(2);
+        return prefs;
+
+
     }
 
     private void setupListeners() {
